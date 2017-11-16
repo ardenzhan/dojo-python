774b32f757bff874b22af05ceb525b4afda28f08
Arden Zhan <arden@protonmail.ch>
2017-11-16 15:15:27 -0600

    implemented templates from Templates section

--- arden_zhan/Django/Django Intro/django_app/apps/blogs_app/views.py
+++ arden_zhan/Django/Django Intro/django_app/apps/blogs_app/views.py
@@ -4,8 +4,13 @@ from __future__ import unicode_literals
 from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
-    response = "placeholder to later display all the list of blogs"
-    return HttpResponse(response)
+    # response = "placeholder to later display all the list of blogs"
+    # return HttpResponse(response)
+    context = {
+        "email": "arden@arden.com",
+        "name": "arden"
+    }
+    return render(request, "blogs_app/index.html", context)
 
 def new(request):
     response = "placeholder to display a new form to create a new blog"
