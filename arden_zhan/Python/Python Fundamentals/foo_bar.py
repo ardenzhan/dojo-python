ff4839d2964fd94201cb96db22e2494f4b85a819
Arden Zhan <arden@protonmail.ch>
2017-11-24 22:03:41 -0600

    foobar assignment, made tables for both squares and primes then printed accordingly. can probably think of better way given more time

--- arden_zhan/Python/Python Fundamentals/foo_bar.py
+++ arden_zhan/Python/Python Fundamentals/foo_bar.py
@@ -3,20 +3,25 @@
 # between 100 and 100,000
 
 min = 100
-max = 150
+max = 1000
 notPrime = []
-for x in range(2, max+1):
-    z = 1
-    while z*z <= x:
-        if z*z == x:
-            print x, "bar"
-        z = z + 1
+isPrime = []
+isSquare = []
 
+#start perfect square finder at min's sqrt
+z = int(min**(0.5))
+while z*z <= max:
+    isSquare.append(z*z)
+    z += 1
+
+#sieve of Eratosthenes for primes
+for x in range(2, max+1):
     if x not in notPrime:
-        print x, "foo"
         for y in range(x*x, max+1, x):
             notPrime.append(y)
-    else:
-        print x, "foobar"
+        isPrime.append(x)
 
-#Sieve of Eratosthenes for primes
+for i in range(min, max+1):
+    if i in isPrime: print i, "foo"
+    elif i in isSquare: print i, "bar"
+    else: print i, "foobar"
